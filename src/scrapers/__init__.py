5b12c8cb885d75e8728f4898a5534f488a0b1dd7
flemingsk <flemingsk@hotmail.com>
2026-03-11 16:18:31 +0100

    Fix init.py imports

--- src/scrapers/__init__.py
+++ src/scrapers/__init__.py
@@ -1,14 +1,21 @@
+from src.scrapers.millesima import MillesimaScraper
 from src.scrapers.vinatis import VinatisScraper
 from src.scrapers.jean_merlaut import JeanMerlautScraper
-# Add other scrapers here
+from src.scrapers.idealwine import IdealwineScraper
+from src.scrapers.lavignery import LaVigneryScraper, VinodisScraper
+from src.scrapers.twil import TwilScraper
+from src.scrapers.cavissima import CavissimaScraper
+from src.scrapers.wine_searcher import WineSearcherScraper
 
 SCRAPERS = {
-    "millesima": MillesimaScraper,
-    "vinatis": VinatisScraper,
-    "jean-merlaut": JeanMerlautScraper,
-    "idealwine": IdealwineScraper,
-    "lavignery": LaVigneryScraper,
-    "twil": TwilScraper,
-    "cavissima": CavissimaScraper,
-    # ...
+    "millesima":     MillesimaScraper,
+    "vinatis":       VinatisScraper,
+    "jean_merlaut":  JeanMerlautScraper,
+    "idealwine":     IdealwineScraper,
+    "lavignery":     LaVigneryScraper,
+    "vinodis":       VinodisScraper,
+    "twil":          TwilScraper,
+    "12bouteilles":  TwilScraper,
+    "cavissima":     CavissimaScraper,
+    "wine-searcher": WineSearcherScraper,
 }
